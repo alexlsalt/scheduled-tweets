f6170d5687d49cbc00bf2141d84b23e281dfce90
alexlsalt <am@alexmortoncreative.com>
2021-02-09 14:43:59 +0100

    Added login, signup, and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
   # GET / contact
   get "contact", to: "contact#index"
 
+  # GET / signup
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
